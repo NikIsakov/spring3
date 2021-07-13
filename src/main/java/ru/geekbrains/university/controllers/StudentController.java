0f88979a571f6c46cb22ff6df6a1625cf0f20fee
nik61088 <nik61088@gmail.com>
2021-07-13 19:23:06 +0300

    Lesson7

---
+++ src/main/java/ru/geekbrains/university/controllers/StudentController.java
@@ -0,0 +1,42 @@
+package ru.geekbrains.university.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+import ru.geekbrains.university.model.Student;
+import ru.geekbrains.university.repositories.StudentRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+@RequiredArgsConstructor
+public class StudentController {
+    private final StudentRepository studentRepository;
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Student getStudentById(@PathVariable Long id) {
+        return studentRepository.findById(id).get();
+    }
+
+    @PostMapping
+    public Student save(@RequestBody Student student) {
+        // student.setId(null);
+        return studentRepository.save(student);
+    }
+
+    @GetMapping("/search_by_name")
+    public Student searchByName(@RequestParam String name) {
+        return studentRepository.findByName(name).get();
+    }
+
+    @GetMapping("/test")
+    public Student testMethod() {
+        return studentRepository.customFinderById(2L, "Bob").get();
+    }
+}
