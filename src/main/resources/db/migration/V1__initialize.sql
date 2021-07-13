eb33cc43fd37a3c7562f76e89b3e20cccff190c9
NikIsakov <nikaarons@gmail.com>
2021-07-13 19:48:26 +0300

    Homework7

--- src/main/resources/db/migration/V1__initialize.sql
+++ src/main/resources/db/migration/V1__initialize.sql
@@ -1,6 +1,12 @@
-create table students (id bigserial primary key, name varchar(255), score int);
-insert into students (name, score) values
-('Bob', 80),
-('John', 70),
-('Jack', 90);
+create table products (id bigserial primary key, title varchar(255), cost int);
+insert into products (title, cost) values
+('Product1', 100),
+('Product2', 200),
+('Product3', 300),
+('Product4', 325),
+('Product5', 480),
+('Product6', 1025),
+('Product7', 375),
+('Product8', 190),
+('Product9', 870);
 
